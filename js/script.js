41742b7ece6254043889dc867faa3e2d3b0338d8
Hamza_Mohamed <hamza.nabil@aurorasdp.com>
2019-04-25 20:42:49 +0200

    Unifox Project

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var firstDiv = document.querySelector('.first');
+var secondDiv = document.querySelector('.second');
+var result = document.querySelector('.result');
+
+function changeColor ( color ) {
+    result.style.background = color
+}
+
